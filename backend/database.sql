f1974d89345a06f0d3043c0015c9cc1471121d3b
IDRISSI O. Ayoub <idrissio.ayoub@gmail.com>
2023-07-11 11:46:22 +0200

    fix : sql file

--- backend/database.sql
+++ backend/database.sql
@@ -1,14 +1,16 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE
+    item (
+        id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        title varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
 
-CREATE TABLE post (
-  `id` int AUTO_INCREMENT NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `created_at` DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY(`id`)
-);
+CREATE TABLE
+    post (
+        id int AUTO_INCREMENT NOT NULL,
+        `title` varchar(255) NOT NULL,
+        `content` text NOT NULL,
+        `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP(),
+        PRIMARY KEY (`id`)
+    );
